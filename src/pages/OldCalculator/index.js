9aefc5ab27f07a75df00d43566b0c671bce7fdf6
Camila Matos <camila.mila148@gmail.com>
2020-10-19 11:49:05 -0300

    Feito layout mobile e desktop

--- src/pages/OldCalculator/index.js
+++ src/pages/OldCalculator/index.js
@@ -14,54 +14,73 @@ function OldCalculator() {
         </div>
         
       </header>
-      <div className='form-birthday'>
-          <label>Informe sua data de nascimento:</label>
-          <form onSubmit={()=>alert('Enviando...')}>
-            
-            <div className='inputs'>
-                <input
-                    className='day' 
-                    type='text'
-                    label='Dia'
-                    name='day'
-                    value='Dia'
-                />
-                <input
-                    className='month' 
-                    type='text'
-                    label='Mês'
-                    name='month'
-                    value='Mês'
-                />
-                <input
-                    className='year' 
-                    type='text'
-                    label='Ano'
-                    name='year'
-                    value='Ano'
-                />
+      <main>
+        <div className="main-container">
+            <div className='form-birthday'>
+                <label>Informe sua data de nascimento:</label>
+                <form onSubmit={()=>alert('Enviando...')}>
+                  
+                  <div className='inputs'>
+                      <input
+                          className='day' 
+                          type='text'
+                          label='Dia'
+                          name='day'
+                          value='Dia'
+                      />
+                      <input
+                          className='month' 
+                          type='text'
+                          label='Mês'
+                          name='month'
+                          value='Mês'
+                      />
+                      <input
+                          className='year' 
+                          type='text'
+                          label='Ano'
+                          name='year'
+                          value='Ano'
+                      />
 
+                  </div>
+
+                
+
+                      <button type='submit' value='calcular'>Calcular</button>
+                </form>
             </div>
+            <div className='result'>
+              <div className="result-container">
+                  <p>Hoje vocễ tem ... anos, ... meses e ... dias!</p>
+                  <div>
+                    <p>Até hoje você já viveu:</p>
+                    <p>... dias</p>
+                    <p>... horas</p>
+                    <p>... minutos</p>
 
-          
+                  </div>
+                  <p className="birthday">Falta ... dias para o seu aniversário!</p>
+              </div>
+            </div>
+            <div className='desc'>
+              <div className="desc-container">
+                  <h2>Seja Bem-Vindo</h2>
+                  <p>
+                    Para descobrir, exatamente quantos anos você tem,
+                    basta colocar a data de nascimento nos campos acima.
+                  </p>
+                  <br />
+                  <p>
+                    Você também ficará sabendo quantos dias, horas e minutos que você já viveu até hoje. 
+                  </p>
 
-                <button type='submit' value='calcular'>Calcular</button>
-          </form>
-      </div>
-      <div className='result'>
-          <p>Hoje vocễ tem ... anos, ... meses e ... dias!</p>
-          <p>Foram ... dias</p>
-          <p>... horas</p>
-          <p>... minutos</p>
-          <p>Falta ... dias para o seu aniversário!</p>
-      </div>
-      <div className='desc'>
-          <h2>Bem-Vindo</h2>
-          <p>
-            Para descobrir, exatamente quantos anos você tem ou outra pessoa,
-             basta colocar a data de nascimento nos campos acima.
-             Você também ficará sabendo quantos dias, horas e minutos já viveu até hoje. </p>
-      </div>
+              </div>
+            </div>
+       
+        </div>
+
+      </main>
     </div>
   );
 }
